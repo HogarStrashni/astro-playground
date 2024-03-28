30afea5adcfddec3f64772d6dfb435c6aa38d8cf
Đorđe Matić <djmatic@agfbl.org>
2024-03-28 12:48:35 +0100

    fix typescript types and add postId slug

--- src/lib/data/queries.ts
+++ src/lib/data/queries.ts
@@ -1,34 +1,40 @@
-const getPosts = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  return res.json();
-};
-
 type Post = {
-  userId: string;
+  userId: number;
   id: number;
   title: string;
   body: string;
 };
 
-const getAuthors = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-  return data.map(({ id, name }) => ({ id, name }));
-};
-
-type Autor = {
+type RandomAuthor = {
   id: number;
   name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+};
+
+const getPosts = async (): Promise<Array<Post>> => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+const getAuthors = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = (await res.json()) as Array<RandomAuthor>;
+  return data.map(({ id, name }) => ({ id, name }));
 };
 
 export const getPostsWithAuthor = async () => {
   const [posts, authors] = await Promise.all([getPosts(), getAuthors()]);
-  return posts.map((posts) => ({
-    id: posts.id,
-    title: posts.title,
-    body: posts.body,
-    author: authors.find(({ id }) => id === posts.userId)?.name,
+  return posts.map((post) => ({
+    id: post.id,
+    title: post.title,
+    body: post.body,
+    author: authors.find(({ id }) => id === post.userId)?.name,
   }));
 };
 
-export type PostWithAuthor = Omit<Post, "userId"> & { author: string };
+export type PostWithAuthor = Awaited<
+  ReturnType<typeof getPostsWithAuthor>
+>[number];
