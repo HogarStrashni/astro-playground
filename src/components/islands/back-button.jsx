fbf061fd440aad0c7a7c33886f811ba7227830b6
Đorđe Matić <djmatic@agfbl.org>
2024-03-29 09:32:43 +0100

    implement BackButton.jsx - integration with react

---
+++ src/components/islands/back-button.jsx
@@ -0,0 +1,15 @@
+import { ArrowLeft } from "lucide-react"
+
+const BackButton = () => {
+  return (
+    <button
+      class="flex items-center space-x-2 rounded-md border px-5 py-2 shadow-sm"
+      onClick={() => window.history.back()}
+     >
+      <ArrowLeft className="h-4 w-4" />
+      <span>Back to all Posts</span>
+    </button>
+  );
+};
+
+export default BackButton;
