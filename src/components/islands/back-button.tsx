6ccb992f0cb6f780b8808341cc79833821c5c911
Đorđe Matić <djmatic@agfbl.org>
2024-04-01 11:14:06 +0200

    small back button improvements

--- src/components/islands/back-button.tsx
+++ src/components/islands/back-button.tsx
@@ -3,7 +3,7 @@ import { ArrowLeft } from "lucide-react";
 const BackButton = () => {
   return (
     <button
-      class="flex items-center space-x-2 rounded-md border px-5 py-2 shadow-sm"
+      className="flex items-center space-x-2 rounded-md border px-5 py-2 shadow-sm hover:bg-gray-50"
       onClick={() => window.history.back()}
     >
       <ArrowLeft className="h-4 w-4" />
