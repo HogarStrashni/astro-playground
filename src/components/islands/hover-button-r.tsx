d054fae56786504cf0a8350fac2e160449289b54
Đorđe Matić <djmatic@agfbl.org>
2024-04-01 11:45:03 +0200

    playing with animation

--- src/components/islands/hover-button-r.tsx
+++ src/components/islands/hover-button-r.tsx
@@ -1,10 +1,11 @@
 import { ArrowRight } from "lucide-react";
+import type { PropsWithChildren } from "react";
 
-const HoverButtonReact = () => {
+const HoverButtonReact = ({ children }: PropsWithChildren) => {
   return (
     <button className="group relative rounded-full border-2 py-2 pl-5">
       <span className="mr-12 overflow-clip bg-gradient-to-r from-white from-50% to-slate-700 to-50% bg-[length:200%_100%] bg-clip-text bg-[left] font-bold text-transparent delay-75 duration-[0.25s] group-hover:bg-[right]">
-        Hello World
+        {children}
       </span>
       <span className="relative">
         <ArrowRight className="absolute right-3 top-1 h-4 w-4 text-slate-700" />
