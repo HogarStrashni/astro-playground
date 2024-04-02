b7883694e9be93da22cfd1fb9a59caa755bf2c04
Đorđe Matić <djmatic@agfbl.org>
2024-04-02 15:27:55 +0200

    add custom navigation to svg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        playground: {
+          "0%": { transform: "scale(1) rotate(10deg)" },
+          "25%": { transform: "scale(1.10) rotate(-10deg)" },
+          "50%": { transform: "scale(1.15) rotate(10deg)" },
+          "75%": { transform: "scale(1.10) rotate(-10deg)" },
+          "100%": { transform: "scale(1) rotate(10deg)" },
+        },
+      },
+      animation: {
+        "svg-playground": "playground 6s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
